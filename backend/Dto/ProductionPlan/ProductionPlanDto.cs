bbd7fee6fc3f83a7bdde15bc05e1d3fb9ba49c06
princeSthaa <shresthaprince222@gmail.com>
2026-07-28 13:05:52 +0545

    plan error

--- backend/Dto/ProductionPlan/ProductionPlanDto.cs
+++ backend/Dto/ProductionPlan/ProductionPlanDto.cs
@@ -1,27 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using backend.Model;
-using backend.Dto.Customer;
-using backend.Dto.Order;
-using backend.Dto.OrderItem;
-using backend.Dto.Product;
-using backend.Dto.Fabric;
-using backend.Dto.WorkCenter;
 using backend.Dto.ProductionPlanProduct;
-using backend.Dto.ProductionPlanProductSize;
 using backend.Dto.ProductionPlanStage;
-using backend.Dto.Material;
-using backend.Dto.BillOfMaterial;
-using backend.Dto.Warehouse;
-using backend.Dto.WarehouseRoom;
-using backend.Dto.WarehouseShelf;
-using backend.Dto.Inventory;
-using backend.Dto.Outlet;
-using backend.Dto.OutletDemand;
-using backend.Dto.Transaction;
-using backend.Dto.ActivityLog;
 using backend.Model.Enums;
 
 namespace backend.Dto.ProductionPlan
